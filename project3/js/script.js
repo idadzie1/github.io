2cc90b5162ccf573ff1e5832178154f5965e9211
idadzie1 <109477712+idadzie1@users.noreply.github.com>
2024-07-15 17:32:08 +0000

    Updated HTML and CSS, added JS folder

---
+++ project3/js/script.js
@@ -0,0 +1,30 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const horizontalbars = document.querySelector('.showmenu');
+    const crossbar = document.querySelector('.closemenu');
+    const sidebar = document.querySelector('.menu__items');
+
+        
+
+        horizontalbars.addEventListener('click', ()=>{
+        horizontalbars.classList.toggle('show');
+        crossbar.classList.toggle('show');
+        sidebar.classList.toggle('slide-side-sidebar');
+
+    })
+
+        crossbar.addEventListener('click', ()=>{
+        horizontalbars.classList.toggle('show');
+        crossbar.classList.toggle('show');
+        sidebar.classList.toggle('slide-side-sidebar');
+        
+    })
+    
+    
+        sidebar.addEventListener('click', ()=>{
+        sidebar.classList.toggle('slide-side-sidebar');
+        horizontalbars.classList.toggle('show');
+        crossbar.classList.toggle('show');
+
+    })
