3f7af7f4102f883851ac66ee09d3112aa029ece1
idadzie1 <109477712+idadzie1@users.noreply.github.com>
2024-07-18 18:30:17 +0000

    updated JS and CSS files

--- project3/js/script.js
+++ project3/js/script.js
@@ -5,30 +5,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const horizontalbars = document.querySelector('.showmenu');
     const crossbar = document.querySelector('.closemenu');
     const sidebar = document.querySelector('.menu__items');
-
-        
-
-        horizontalbars.addEventListener('click', ()=>{
+    const buttonmenu = document.querySelector('.nav__clickbtn');
+    
+    const action = ()=>{
         horizontalbars.classList.toggle('show');
         crossbar.classList.toggle('show');
         sidebar.classList.toggle('slide-side-sidebar');
+        buttonmenu.classList.toggle('slidenav__clickbtn');
+    }
 
+        horizontalbars.addEventListener('click', ()=>{        
+        action();
     });
 
-        crossbar.addEventListener('click', ()=>{
-        horizontalbars.classList.toggle('show');
-        crossbar.classList.toggle('show');
-        sidebar.classList.toggle('slide-side-sidebar');
-        
+        crossbar.addEventListener('click', ()=>{        
+        action();
     });
     
     
-        sidebar.addEventListener('click', ()=>{
-        sidebar.classList.toggle('slide-side-sidebar');
-        horizontalbars.classList.toggle('show');
-        crossbar.classList.toggle('show');
-
+        sidebar.addEventListener('click', ()=>{        
+        action();
     });
+
+       buttonmenu.addEventListener('click', ()=>{       
+        action();
+       });
+
 
     window.addEventListener('scroll', ()=>{
         document.querySelector('nav').classList.toggle('scrolled', window.scrollY > 0);
