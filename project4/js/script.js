f836bc77a78784702aea256a0443bd13ea021c13
idadzie1 <109477712+idadzie1@users.noreply.github.com>
2024-08-08 19:46:48 +0000

    updated index.html, style.css and script.js

--- project4/js/script.js
+++ project4/js/script.js
@@ -3,20 +3,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     window.addEventListener('scroll', ()=>{
         document.querySelector('nav').classList.toggle('scrolled', window.scrollY > 0);
-    });    
-
+    });
+    
+    const menuLinks = document.querySelectorAll('nav .container .pwr ul a');    
+    menuLinks.forEach(menuLink =>{        
+        menuLink.addEventListener('click', ()=>{
+            menuLinks.forEach(link => link.classList.remove('active'));
+            menuLink.classList.add('active');            
+        });
+    });
 
     const hamburgerIcon = document.querySelector('.humburherIcon');
     const menuItems = document.querySelector('nav .container .pwr ul');
-
     hamburgerIcon.addEventListener('click', ()=>{        
-        menuItems.classList.toggle('showOnClick');
-        
+        menuItems.classList.toggle('showOnClick');        
     });
 
     menuItems.addEventListener('click', ()=>{
         menuItems.classList.toggle('showOnClick');
-    })    
+    });    
   
 
     // ========= FQA ===========
